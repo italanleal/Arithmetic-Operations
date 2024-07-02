b9f3c402024100f7f019a9cf68d6893a9d485c7a
italan <italanleal@gmail.com>
2024-07-01 21:11:09 -0300

    Update MathExamplesTest.java

--- src/test/java/br/upe/MathExamplesTest.java
+++ src/test/java/br/upe/MathExamplesTest.java
@@ -7,20 +7,9 @@ import org.junit.jupiter.api.Test;
 public class MathExamplesTest
 {
     @Test
-    public void givenEmptyArity_whenAdd_thenReturnZero()
-    {
-        assertEquals(MathExamples.add(), 0);
-    }
-
-    @Test
     public void givenRegularEntry_whenAdd_thenReturnSumOfElements()
     {
-        assertEquals(MathExamples.add(1, 2, 3), 6);
-    }
-    @Test
-    public void givenSingleNumber_whenAdd_thenReturnNumber()
-    {
-        assertEquals(MathExamples.add(11), 11);
+        assertEquals(MathExamples.add(1, 2), 3);
     }
 
     @Test
